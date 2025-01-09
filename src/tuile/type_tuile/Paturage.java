af2d8afff63bef5956da3e36748ea0daced04341
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:43:12 +0100

    Constructeur de la class Montagne

--- src/tuile/type_tuile/Paturage.java
+++ src/tuile/type_tuile/Paturage.java
@@ -3,6 +3,10 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Paturage extends Tuile{
-
-    
+    /**
+     * Constructeur de la classe Paturage.
+     */
+    public Paturage() {
+        super("Paturage");
+    }
 }
