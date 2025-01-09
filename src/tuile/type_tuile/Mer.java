5119f2b806795c466a9d30176381ef2116936692
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:41:04 +0100

    Constructeur de la class Mer

--- src/tuile/type_tuile/Mer.java
+++ src/tuile/type_tuile/Mer.java
@@ -3,5 +3,10 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Mer extends Tuile{
-
+    /**
+     * Constructeur de la classe Mer.
+     */
+    public Mer() {
+        super("Mer");
+    }
 }
