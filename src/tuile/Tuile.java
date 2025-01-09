c8645c315880432a1da4be31141b6118844c3987
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:32:31 +0100

    Constructeur de la class Tuile

--- src/tuile/Tuile.java
+++ src/tuile/Tuile.java
@@ -1,5 +1,15 @@
 package tuile;
 
-public class Tuile {
-    
+public abstract class Tuile {
+
+    protected String type;
+
+    /**
+     * Constructeur de la classe Tuile.
+     * 
+     * @param type le type de la tuile
+     */
+    public Tuile(String type) {
+        this.type = type;
+    }
 }
