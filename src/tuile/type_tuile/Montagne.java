9695e2b1b76254d88d82ca8bd4104f8c3c1a5999
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:42:34 +0100

    Constructeur de la class Montagne

--- src/tuile/type_tuile/Montagne.java
+++ src/tuile/type_tuile/Montagne.java
@@ -3,5 +3,10 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Montagne extends Tuile{
-
+    /**
+     * Constructeur de la classe Montagne.
+     */
+    public Montagne() {
+        super("Montagne");
+    }
 }
