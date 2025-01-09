f7d166e65a0e2cb59d3c72f6f665e0c8fb7c8f0b
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-09 18:22:08 +0100

    creation des attribu de Plateau

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -1,5 +1,17 @@
 package plateau;
 
 public class Plateau {
+	/**
+	 * en attendant la creation de la classe tuile je l'ai mit en commentaire
+	 */
+	//private Tuilles[][] tuilles ;
+	private int x ;
+	private int y;
+	private int nbMers;
+    private int nbChamps;
+    private int nbForets;
+    private int nbMontagnes;
+    private int nbPaturages;
+	
 
 }
