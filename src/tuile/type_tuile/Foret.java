cadbf13f2145468984ebe7c9ee869af38d614962
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:41:51 +0100

    Constructeur de la class Foret

--- src/tuile/type_tuile/Foret.java
+++ src/tuile/type_tuile/Foret.java
@@ -3,5 +3,10 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Foret extends Tuile{
-
+    /**
+     * Constructeur de la classe Foret.
+     */
+    public Foret() {
+        super("Foret");
+    }
 }
