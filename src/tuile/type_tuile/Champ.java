928ffdd3f439099366afb66ae401140724f09531
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-09 23:38:55 +0100

    Constructeur de la class Champ

--- src/tuile/type_tuile/Champ.java
+++ src/tuile/type_tuile/Champ.java
@@ -3,5 +3,10 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Champ extends Tuile {
-
+    /**
+     * Constructeur de la classe Champ.
+     */
+    public Champ() {
+        super("Champ");
+    }
 }
