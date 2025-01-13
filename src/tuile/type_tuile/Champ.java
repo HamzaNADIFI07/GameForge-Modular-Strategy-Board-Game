ff9db14489a1c5c96d960e915fe4a38d35868202
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-13 01:22:36 +0100

    adaptation de la classe champ à la classe ressource

--- src/tuile/type_tuile/Champ.java
+++ src/tuile/type_tuile/Champ.java
@@ -3,10 +3,17 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Champ extends Tuile {
+	private Ressource ressource;
+
     /**
      * Constructeur de la classe Champ.
      */
-	public Champ(int x, int y) {
-        super("Champ", x, y);
-    }
+	    public Champ(int x, int y) {
+	        super("Champ", x, y);
+	        this.ressource = new Ressource("Blé",1);
+	    }
+
+	    public Ressource produireRessource() {
+	        return ressource;
+	    }
 }
