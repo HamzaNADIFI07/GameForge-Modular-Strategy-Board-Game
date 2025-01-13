2c1f4927ad5a90d638e15a0b93801f01f99f065b
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-13 01:20:00 +0100

    creation d'une class Ressource

---
+++ src/tuile/type_tuile/Ressource.java
@@ -0,0 +1,21 @@
+package tuile.type_tuile;
+
+public class Ressource {
+	 private String type;
+	    private int quantite;
+
+	    public Ressource(String type, int quantite) {
+	        this.type = type;
+	        this.quantite = quantite;
+	    }
+
+	    public String getType() {
+	        return type;
+	    }
+
+	    public int getQuantite() {
+	        return quantite;
+	    }
+
+
+}
