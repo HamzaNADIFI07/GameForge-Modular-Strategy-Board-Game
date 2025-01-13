e1843b1cd459d3d0f10f4ce1a6c471bc99fe7ba8
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-13 01:25:25 +0100

    modification de la classe Montagne

--- src/tuile/type_tuile/Montagne.java
+++ src/tuile/type_tuile/Montagne.java
@@ -3,8 +3,14 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Montagne extends Tuile {
+	private Ressource ressource;
+
     public Montagne(int x, int y) {
         super("Montagne", x, y);
+        this.ressource = new Ressource("Minerai", 1);
     }
 
+    public Ressource produireRessource() {
+        return ressource;
+    }
 }
