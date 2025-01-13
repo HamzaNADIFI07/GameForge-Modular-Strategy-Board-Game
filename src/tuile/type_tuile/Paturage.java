a68e1c1fc3dada756f30a54031cb199e5138016d
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-13 01:24:30 +0100

    adaptation de la constructeur de champ à la classe ressource

--- src/tuile/type_tuile/Paturage.java
+++ src/tuile/type_tuile/Paturage.java
@@ -3,11 +3,17 @@ package tuile.type_tuile;
 import tuile.Tuile;
 
 public class Paturage extends Tuile{
+	private Ressource ressource;
     /**
      * Constructeur de la classe Paturage.
      */
-    public Paturage(int x,int y) {
-        super("Paturage",x,y);
+    public Paturage(int x, int y) {
+        super("Pâturage", x, y);
+        this.ressource = new Ressource("Mouton", 1);
+    }
+
+    public Ressource produireRessource() {
+        return ressource;
     }
     
 }
