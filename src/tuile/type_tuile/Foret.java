07aa0a9098bc792138c81759726ef4ecdaacec84
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-01-15 11:02:15 +0100

    modification de classe Foret

--- src/tuile/type_tuile/Foret.java
+++ src/tuile/type_tuile/Foret.java
@@ -1,11 +1,18 @@
 package tuile.type_tuile;
 
+import ressource.Ressource;
 import tuile.Tuile;
 
 public class Foret extends Tuile{
+    private Ressource ressource;
     /**
      * Constructeur de la classe Foret.
      */
 	  public Foret(int x, int y) {
 	      super("Forêt", x, y);
-}}
+}
+      public Ressource produireRessource() {
+      return ressource;
+}
+
+}
