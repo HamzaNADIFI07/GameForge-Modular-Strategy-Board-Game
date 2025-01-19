732bbbd126bb5b0a7f75802c274ac7ee3b70c940
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-20 00:20:51 +0100

    javadoc de la class enumRessource

--- src/ressource/enumRessource.java
+++ src/ressource/enumRessource.java
@@ -1,5 +1,9 @@
 package ressource;
 
+/**
+ * L'énumération enumRessource représente les différents types de ressources disponibles dans le jeu.
+ * Chaque ressource correspond à un type de tuile spécifique qui peut la produire.
+ */
 public enum enumRessource { 
     Ble, Minerai, Bois, Moutons;  
 }
