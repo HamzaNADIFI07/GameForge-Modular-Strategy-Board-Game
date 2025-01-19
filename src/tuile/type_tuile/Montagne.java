2d1367e3a21e7f2e1318bf4aa86257c1f57b7b30
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-20 00:33:53 +0100

    javadoc de la class Montagne

--- src/tuile/type_tuile/Montagne.java
+++ src/tuile/type_tuile/Montagne.java
@@ -2,15 +2,31 @@ package tuile.type_tuile;
 
 import ressource.enumRessource;
 import tuile.Tuile;
-
+/**
+ * La classe Montagne représente une tuile de type montagne dans le jeu.
+ */
 public class Montagne extends Tuile {
+
 	private final enumRessource ressource;
 
+    /**
+     * Constructeur de la classe Montagne.
+     * Initialise une tuile de type Montagne avec des coordonnées spécifiques.
+     * La ressource produite par une montagne est toujours du minerai.
+     * 
+     * @param x la coordonnée x de la tuile
+     * @param y la coordonnée y de la tuile
+     */
     public Montagne(int x, int y) {
         super("Montagne", x, y);
         this.ressource = enumRessource.Minerai;
     }
 
+    /**
+     * Retourne la ressource produite par cette tuile (minerai).
+     * 
+     * @return la ressource produite par la tuile (enumRessource.Minerai)
+     */
     public enumRessource produireRessource() {
         return ressource;
     }
