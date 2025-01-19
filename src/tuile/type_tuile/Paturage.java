fcdab1af5426bfdf1230ec9b361aac9a7ad4cd8e
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-20 00:36:22 +0100

    javadoc de la class Paturage

--- src/tuile/type_tuile/Paturage.java
+++ src/tuile/type_tuile/Paturage.java
@@ -3,16 +3,32 @@ package tuile.type_tuile;
 import ressource.enumRessource;
 import tuile.Tuile;
 
+/**
+ * La classe Paturage représente une tuile de type pâturage dans le jeu.
+ */
 public class Paturage extends Tuile{
+
 	private final enumRessource ressource;
+
+
     /**
      * Constructeur de la classe Paturage.
+     * Initialise une tuile de type Pâturage avec des coordonnées spécifiques.
+     * La ressource produite par un pâturage est toujours des moutons.
+     * 
+     * @param x la coordonnée x de la tuile
+     * @param y la coordonnée y de la tuile
      */
     public Paturage(int x, int y) {
         super("Pâturage", x, y);
         this.ressource = enumRessource.Moutons;
     }
 
+    /**
+     * Retourne la ressource produite par cette tuile (moutons).
+     * 
+     * @return la ressource produite par la tuile (enumRessource.Moutons)
+     */
     public enumRessource produireRessource() {
         return ressource;
     }
