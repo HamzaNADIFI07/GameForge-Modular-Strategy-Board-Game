57cafc50f2aad4092dc6bd8cafbab3b76d2d32d5
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-20 00:31:57 +0100

    javadoc de la class Mer

--- src/tuile/type_tuile/Mer.java
+++ src/tuile/type_tuile/Mer.java
@@ -2,11 +2,23 @@ package tuile.type_tuile;
 
 import tuile.Tuile;
 
+/**
+ * La classe Mer représente une tuile de type mer dans le jeu.
+ * Les tuiles de type mer ne produisent aucune ressource et ne peuvent pas contenir de bâtiment.
+ */
 public class Mer extends Tuile{
+
+
     /**
      * Constructeur de la classe Mer.
+     * Initialise une tuile de type Mer avec des coordonnées spécifiques.
+     * 
+     * @param x la coordonnée x de la tuile
+     * @param y la coordonnée y de la tuile
      */
     public Mer(int x, int y) {
         super("Mer", x, y);
     }
+
+    
 }
