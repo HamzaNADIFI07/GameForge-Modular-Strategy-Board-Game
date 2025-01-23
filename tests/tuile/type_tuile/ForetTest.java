9b85654826963e219cb1068f44cb8724d44bea9f
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 08:23:19 +0100

    classe pour tester le foret

---
+++ tests/tuile/type_tuile/ForetTest.java
@@ -0,0 +1,14 @@
+package tuile.type_tuile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ForetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
