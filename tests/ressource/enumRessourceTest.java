1cce179be85dd16284683973edc045d7b1783d35
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 08:21:14 +0100

    creation des classes pour tester le classe ressource et le classe tuile

---
+++ tests/ressource/enumRessourceTest.java
@@ -0,0 +1,14 @@
+package ressource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class enumRessourceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/tuile/TuileTest.java
@@ -0,0 +1,14 @@
+package tuile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TuileTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
