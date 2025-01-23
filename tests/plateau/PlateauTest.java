c815b0fdcbeb06ce49a9a3bae382270a871d7de4
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 17:31:35 +0100

    ajout d'un test dans tuile

--- tests/plateau/PlateauTest.java
+++ tests/plateau/PlateauTest.java
@@ -8,34 +8,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PlateauTest{
 	
-private Plateau plateau;
+	private Plateau plateau;
 
-@BeforeEach
-public void setup() {
-    plateau = new Plateau(10, 10); // Plateau 10x10
+	@BeforeEach
+	public void setup() {
+		plateau = new Plateau(10, 10); // Plateau 10x10
+	}
+		@Test
+	public void testGenererTuiles() {
+		plateau.genererTuiles();
+		int nombreTuileMer = plateau.getNbTuilesMer();
+		int nbTuiles = plateau.getX() * plateau.getY();
+		assertTrue(nombreTuileMer>=2*nbTuiles/3, 
+				"Le nombre de tuiles non-Mer doit être égal à 1/3 du nombre total de tuiles.");
 }
-@Test
-public void testGenererTuiles() {
-    plateau.genererTuiles();
-    int nombreTuileMer = plateau.getNbTuilesMer();
-    int nbTuiles = plateau.getX() * plateau.getY();
-    assertTrue(nombreTuileMer>=2*nbTuiles/3, 
-        "Le nombre de tuiles non-Mer doit être égal à 1/3 du nombre total de tuiles.");
-}
-@Test
-public void testPlateauInitialiseAvecMer() {
-    // Vérifie que le plateau est bien initialisé avec uniquement des tuiles de type Mer
-    boolean toutEstMer = true;
+		@Test
+		public void testPlateauInitialiseAvecMer() {
+    	// Vérifie que le plateau est bien initialisé avec uniquement des tuiles de type Mer
+    	boolean toutEstMer = true;
 
-    for (int i = 0; i < plateau.getX(); i++) {
-        for (int j = 0; j < plateau.getY(); j++) {
-            if (!(plateau.getTuile(i, j) instanceof Mer)) {
-                toutEstMer = false;
+    	for (int i = 0; i < plateau.getX(); i++) {
+    		for (int j = 0; j < plateau.getY(); j++) {
+    			if (!(plateau.getTuile(i, j) instanceof Mer)) {
+    				toutEstMer = false;
             }
         }
     }
 
-    assertTrue(toutEstMer, "Tous les éléments du plateau doivent être des tuiles de type Mer au début.");
+    	assertTrue(toutEstMer, "Tous les éléments du plateau doivent être des tuiles de type Mer au début.");
 }
 
 
--- tests/tuile/TuileTest.java
+++ tests/tuile/TuileTest.java
@@ -1,14 +1,16 @@
 package tuile;
+import plateau.Plateau;
+import tuile.type_tuile.Mer;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 class TuileTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private Tuile tuile;
 
+    @BeforeEach
+    public void setup() {
+        tuile = new TuileTestConcrete("Foret", 5, 7);
+    }
 }
