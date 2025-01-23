8267709f22738e3789af98e935e0cd5325fa214d
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 18:17:12 +0100

    changement de la méthode pour tester le type de tuile

--- tests/tuile/TuileTest.java
+++ tests/tuile/TuileTest.java
@@ -1,21 +1,21 @@
 package tuile;
 import plateau.Plateau;
 import tuile.type_tuile.Mer;
+import tuile.type_tuile.Champ;
+
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class TuileTest {
 
-    private Tuile tuile;
-
-    @BeforeEach
-    public void setup() {
-        Tuile tuile = new Mer(0, 0); 
-        assertEquals("Mer", tuile.getType());
-    }
 @Test
 public void testGetType() {
-    assertEquals("Mer", tuile.getType(), "Le type de la tuile doit être 'Mer'.");
-}}
+	Tuile mer = new Mer(0, 0); 
+    Tuile champ = new Champ(3, 2);
+    assertEquals("Mer", mer.getType(), "Le type de la tuile doit être 'Mer'.");
+    assertEquals("Champ", champ.getType(), "Le type de la tuile doit être 'champ'.");
 
+}
+}
