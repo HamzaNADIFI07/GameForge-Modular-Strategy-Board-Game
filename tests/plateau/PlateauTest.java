f3fdc9a372eb1fc3163a2983c6758ecf3919cca4
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 08:44:37 +0100

    méthode pour tester la generation des tuiles

--- tests/plateau/PlateauTest.java
+++ tests/plateau/PlateauTest.java
@@ -1,5 +1,5 @@
 package plateau;
-
+import plateau.Plateau;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +12,12 @@ private Plateau plateau;
 public void setup() {
     plateau = new Plateau(10, 10); // Plateau 10x10
 }
-
+@Test
+public void testGenererTuiles() {
+    plateau.genererTuiles();
+    int nombreTuilesNonMer = plateau.getNbTuilesNonMer();
+    assertEquals(plateau.getX() * plateau.getY() / 3, nombreTuilesNonMer, 
+        "Le nombre de tuiles non-Mer doit être égal à 1/3 du nombre total de tuiles.");
+}
 
 }
