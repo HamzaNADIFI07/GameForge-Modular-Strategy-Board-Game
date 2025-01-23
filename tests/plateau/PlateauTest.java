7da9fc2b35449160d39241dbc24cac42463a0726
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-01-23 16:15:34 +0100

    ajout d'une méthode pour tester si le plateau s'initialise avec les tuiles mer

--- tests/plateau/PlateauTest.java
+++ tests/plateau/PlateauTest.java
@@ -1,5 +1,7 @@
 package plateau;
 import plateau.Plateau;
+import tuile.type_tuile.Mer;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +21,21 @@ public void testGenererTuiles() {
     assertEquals(plateau.getX() * plateau.getY() / 3, nombreTuilesNonMer, 
         "Le nombre de tuiles non-Mer doit être égal à 1/3 du nombre total de tuiles.");
 }
+@Test
+public void testPlateauInitialiseAvecMer() {
+    // Vérifie que le plateau est bien initialisé avec uniquement des tuiles de type Mer
+    boolean toutEstMer = true;
+
+    for (int i = 0; i < plateau.getX(); i++) {
+        for (int j = 0; j < plateau.getY(); j++) {
+            if (!(plateau.getTuile(i, j) instanceof Mer)) {
+                toutEstMer = false;
+            }
+        }
+    }
+
+    assertTrue(toutEstMer, "Tous les éléments du plateau doivent être des tuiles de type Mer au début.");
+}
+
 
 }
