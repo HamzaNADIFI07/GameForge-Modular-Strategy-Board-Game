0791c78eab24f92298ef82d809ec3b2ee17a6835
Hamza NADIFI <nadifihamza07@gmail.com>
2025-01-25 16:27:56 +0100

    attributs et constructeur de la class Batiment

--- src/batiment/Batiment.java
+++ src/batiment/Batiment.java
@@ -1,8 +1,23 @@
 package batiment;
 
 public abstract class Batiment {
- public abstract int getDimension();
+    protected String type;
+    protected int dimension;
 
- public abstract int setDimension();  /** sous reserve */
+
+    /**
+     * Constructeur de la classe Batiment.
+     * 
+     * @param nom le nom du bâtiment
+     * @param taille la taille ou la capacité du bâtiment
+     */
+    public Batiment(String type, int dimension) {
+        this.type = type;
+        this.dimension = dimension;
+    }
+
+    public abstract int getDimension();
+
+    public abstract int setDimension();  /** sous reserve */
 }
 
