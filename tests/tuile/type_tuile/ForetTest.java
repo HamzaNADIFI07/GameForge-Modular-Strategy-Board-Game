f8b8a833870a11054ad0e7d322f3b420c01d4685
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-01-26 22:19:36 +0100

    ajout methode de test du classe Foret

--- tests/tuile/type_tuile/ForetTest.java
+++ tests/tuile/type_tuile/ForetTest.java
@@ -1,14 +1,27 @@
 package tuile.type_tuile;
+import tuile.type_tuile.Foret;
+import ressource.enumRessource; 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class ForetTest {
+public class ForetTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void ForetTest() {
+		int x = 10;
+		int y = 20;
+		Foret foret = new Foret(x, y);
+
+		// Verifie si les coordonnées sont bien initialisées
+		assertEquals(x, foret.getX());
+		assretEquals(y, foret.getY());
+
+		// Verifier si la ressource est correctement initialisée à enumRessource.Bois
+		assertEquals(enumRessource.Bois, foret.getRessource());
+
 	}
 
 }
