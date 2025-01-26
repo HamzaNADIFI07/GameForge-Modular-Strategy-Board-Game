e8b34efff4c4233f1ee407e7568e12cff0baa145
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-01-26 21:44:35 +0100

    ajout du methode de test de la classe champ

--- tests/tuile/type_tuile/ChampTest.java
+++ tests/tuile/type_tuile/ChampTest.java
@@ -1,14 +1,31 @@
 package tuile.type_tuile;
+import tuile.type_tuile.Champ;
+import ressource.enumRessource;
+import tuile.Tuile;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class ChampTest {
+import java.util.List;
+
+public class ChampTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void test() {
+		// on crée une instance de champs avec des coordonnées(x, y)
+
+		int x = 15;
+		int y = 30;
+		Champ champ = new Champ(x, y);
+
+		// Verifie si les coordonnées sont bien initialisées
+		assertEquals(x, champ.getX());
+		assertEquals(y, champ.getY());
+
+		// Verifie si la ressource est correctement initialisée à enumRessource.Ble
+		assertEquals(enumRessource.Ble, champ.getRessource());
 	}
 
 }
