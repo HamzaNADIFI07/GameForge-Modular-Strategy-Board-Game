f46a364db4cfef2f09c7032a70fd15eef49bfc3d
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-01-27 00:17:55 +0100

    test pour la classe Paturage

--- tests/tuile/type_tuile/PaturageTest.java
+++ tests/tuile/type_tuile/PaturageTest.java
@@ -1,5 +1,9 @@
 package tuile.type_tuile;
+import tuile.type_tuile.Paturage;
+import ressource.enumRessource;
+import tuile.Tuile;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +11,22 @@ import org.junit.jupiter.api.Test;
 class PaturageTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testPaturageConstructor() {
+		// creation d'une instance de Paturage
+		int x = 10;
+		int y = 20;
+		Paturage paturage = new Paturage(x, y);
+
+		// Verifie le nom est correctement Paturage
+		assertEquals("Paturage", paturage.getType());
+
+		//Verifie si les coordonnées sont bien initialisées
+		assertEquals(x, paturage.getX());
+		assertEquals(y, paturage.getY());
+
+		// Verifie si la ressource est Moutons
+		assertEquals(enumRessource.Moutons, paturage.produireRessource());
+		
 	}
 
 }
