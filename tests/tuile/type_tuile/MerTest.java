dab7a40fb64ef51953eb0a3bd695e54a5748ee37
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-01-26 23:10:31 +0100

    ecriture du test de la classe Mer

--- tests/tuile/type_tuile/MerTest.java
+++ tests/tuile/type_tuile/MerTest.java
@@ -1,14 +1,21 @@
 package tuile.type_tuile;
+import tuile.type_tuile.Mer;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class MerTest {
+public class MerTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testMerConstructor() {
+		// créer une instance de mer
+		Mer mer = new Mer(10, 20);
+
+		// Verifier que les valeurs sont initialisées
+		assertEquals(15, mer.getX());
+        assertEquals(20, mer.getY());
 	}
 
 }
