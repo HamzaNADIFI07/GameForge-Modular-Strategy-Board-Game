87a5a2bc8c37e7ab2b68a2cc1ff98cb89843d0ca
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-11 11:58:04 +0100

    changement de peutEtreConstruit

--- src/batiment/type_batiment/Ferme.java
+++ src/batiment/type_batiment/Ferme.java
@@ -13,13 +13,10 @@ public class Ferme extends Batiment{
     }
     /**
      * Vérifie si le bâtiment peut être construit.
-     * 
      */
-    // Pour l'instant dans cette  méthode, on va implémenter juste la contrainte du fait qu'une ferme ne peut pas être construite sur une tuile de type Mer.
-    // Pour les containte des ressources, on l'implémentera directement dans les méthodes d'action construireFerme()
     @Override
     public boolean peutEtreConstruit() {
-        if (this.getTuile().getType().equals("Mer")) {
+        if (this.getTuile() != null && this.getTuile().getType().equals("Mer")) {
             throw new IllegalArgumentException("Une ferme ne peut pas être construite sur une tuile de type Mer.");
         }
         return true;
