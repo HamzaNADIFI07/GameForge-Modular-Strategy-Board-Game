31ff65f736f4b22206db12fa6135cdcf49c54107
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-11 11:59:44 +0100

    implementation de méthode getcout et importation des types

--- src/batiment/type_batiment/Ferme.java
+++ src/batiment/type_batiment/Ferme.java
@@ -1,8 +1,10 @@
 package batiment.type_batiment;
 
 import batiment.Batiment;
+import ressource.enumRessource;
 import tuile.Tuile;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Ferme extends Batiment{
 
     /**
@@ -11,6 +13,14 @@ public class Ferme extends Batiment{
     public Ferme(Tuile tuile) {
         super("Ferme", 1, tuile);
     }
+    
+    public Map<enumRessource, Integer> getCout() {
+        Map<enumRessource, Integer> cout = new HashMap<>();
+        cout.put(enumRessource.Bois, 1);  // Bois : 1
+        cout.put(enumRessource.Minerai, 1);  // Minerai : 1
+        return cout;
+    }
+
     /**
      * Vérifie si le bâtiment peut être construit.
      */
