5979ec98f1d5fdd8375edbe5e89620b89d77b459
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-12 11:14:14 +0100

    prend deux arguments au min 10

--- src/main/Livrable2.java
+++ src/main/Livrable2.java
@@ -1,1 +1,13 @@
+package main;
 
+
+public class Livrable2 {
+	public static void main1(String[] args) {
+		if (args.length <2) {
+			System.out.println("Usage: java Livrable2 <largeur> <longeur>");
+			return;
+		}
+		int largeur = Math.max(10,Integer.parseInt(args[0]));
+		int longeur = Math.max(10,Integer.parseInt(args[0]));
+	}
+}
