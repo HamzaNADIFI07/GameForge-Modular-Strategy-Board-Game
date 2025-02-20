99a09df3829df843f9a604206017015b0eabbc6b
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 03:26:49 +0100

    test de la creation de port

--- tests/batimentTypeTests/PortTest.java
+++ tests/batimentTypeTests/PortTest.java
@@ -1,14 +1,23 @@
 package batimentTypeTests;
+import tuile.Tuile;
+import tuile.type_tuile.Champ;
+import tuile.type_tuile.Mer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class PortTest {
+import batiment.type_batiment.Port;
+public class PortTest {
+
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testPortCreation() {
+		Tuile champTuile = new Champ(3, 2);
+		Port port = new Port(champTuile);
+		assertNotNull(port);
+		assertEquals("Port", port.getType());
+		assertEquals(champTuile, port.getTuile());
 	}
 
 }
