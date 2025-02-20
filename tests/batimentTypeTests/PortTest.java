37ab87acc2566f0f5a2ae9a2960f9f070bd7bab5
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 03:41:05 +0100

    tester si le port peuvent etre construit

--- tests/batimentTypeTests/PortTest.java
+++ tests/batimentTypeTests/PortTest.java
@@ -5,6 +5,7 @@ import tuile.type_tuile.Mer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -30,5 +31,13 @@ public class PortTest {
 		assertEquals(1, cout.get(enumRessource.Bois), "Un port coûte 1 bois");
 		assertEquals(2, cout.get(enumRessource.Moutons), "Un port coûte 2 moutons");
 	}
-
+	@Test
+	public void testPortPeutEtreConstruit() {
+		Tuile merTuile = new Mer(3, 3);
+		Tuile champTuile = new Champ(3, 2);
+		List<Tuile> champAdjacents = champTuile.getAdjacents();
+		champAdjacents.add(merTuile);
+		Port port = new Port(champTuile);
+		assertTrue(port.peutEtreConstruit(), "Un port doit être adjacent à une tuile Mer");
+	}
 }
