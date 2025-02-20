1a85a0e5e604b0ea8b35624832459c96997225c2
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 02:41:55 +0100

    méthode pour tester la creation d'armée

--- tests/batimentTypeTests/ArmeeTest.java
+++ tests/batimentTypeTests/ArmeeTest.java
@@ -1,14 +1,28 @@
 package batimentTypeTests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import batiment.type_batiment.Armee;
 import org.junit.jupiter.api.Test;
+import tuile.Tuile;
+import tuile.type_tuile.Paturage;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ArmeeTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void testArmeeCreation() {
+        Tuile tuile = new Paturage(1, 1);
+        Armee armee = new Armee(tuile, 3);
+        
+        assertEquals("Armee", armee.getType());
+        assertEquals(3, armee.getDimension());
+        assertEquals(tuile, armee.getTuile());
+        assertThrows(IllegalArgumentException.class, () -> new Armee(tuile, 6), "Armée doit avoir entre 1 et 5 guerriers");
+    }
+    
+   
+
 
 }
+
