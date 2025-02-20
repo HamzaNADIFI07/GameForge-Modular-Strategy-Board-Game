b1521e4b5adf957230bfe5b2397a203e034634f5
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 03:22:35 +0100

    creation de la classe pour tester le port

---
+++ tests/batimentTypeTests/PortTest.java
@@ -0,0 +1,14 @@
+package batimentTypeTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PortTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
