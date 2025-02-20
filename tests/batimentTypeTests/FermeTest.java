0ec547cb6ade98586e48bd09e56830ff9a5222ed
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 02:58:16 +0100

    creation de la classe pour tester la ferme

---
+++ tests/batimentTypeTests/FermeTest.java
@@ -0,0 +1,14 @@
+package batimentTypeTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FermeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
