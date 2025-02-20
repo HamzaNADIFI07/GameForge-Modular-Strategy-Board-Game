b1cd7362c5e62f7408607ba9a3c41bda3aa41704
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 03:18:03 +0100

    méthode pour assurer le cout est correct

--- tests/batimentTypeTests/FermeTest.java
+++ tests/batimentTypeTests/FermeTest.java
@@ -3,9 +3,12 @@ package batimentTypeTests;
 import org.junit.jupiter.api.Test;
 
 import batiment.type_batiment.Ferme;
+import ressource.enumRessource;
 import tuile.type_tuile.Champ;
 import tuile.Tuile;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
 
 public class FermeTest {
 
@@ -15,6 +18,14 @@ public class FermeTest {
     	Ferme ferme = new Ferme(champTuile);
     	assertEquals("Ferme", ferme.getType());
     	assertEquals(champTuile, ferme.getTuile());
+    }
+    @Test
+    public void testFermeCout() {
+    	Tuile champTuile = new Champ(2, 2);
+    	Ferme ferme = new Ferme(champTuile);
+    	Map<enumRessource, Integer> cout = ferme.getCout();
+    	assertEquals(1, cout.get(enumRessource.Bois), "Une ferme coûte 1 bois");
+    	assertEquals(1, cout.get(enumRessource.Minerai), "Une ferme coûte 1 minerai");
     }
 }
 
