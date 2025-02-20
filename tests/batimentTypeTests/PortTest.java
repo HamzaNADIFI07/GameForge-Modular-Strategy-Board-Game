47612422d756be19141e69322759bd0cbfd7bec6
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 03:30:29 +0100

    verifie le cout de la port

--- tests/batimentTypeTests/PortTest.java
+++ tests/batimentTypeTests/PortTest.java
@@ -5,9 +5,12 @@ import tuile.type_tuile.Mer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import batiment.type_batiment.Port;
+import ressource.enumRessource;
 public class PortTest {
 
 
@@ -18,6 +21,14 @@ public class PortTest {
 		assertNotNull(port);
 		assertEquals("Port", port.getType());
 		assertEquals(champTuile, port.getTuile());
+	}
+	@Test
+	public void testPortCout() {
+		Tuile champTuile = new Champ(3, 2);
+		Port port = new Port(champTuile);
+		Map<enumRessource, Integer> cout = port.getCout();
+		assertEquals(1, cout.get(enumRessource.Bois), "Un port coûte 1 bois");
+		assertEquals(2, cout.get(enumRessource.Moutons), "Un port coûte 2 moutons");
 	}
 
 }
