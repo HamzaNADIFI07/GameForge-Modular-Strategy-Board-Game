e720c8067de696d981774c8886a836df03f56515
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-20 02:56:07 +0100

    verifie le cout de construction d'un armee

--- tests/batimentTypeTests/ArmeeTest.java
+++ tests/batimentTypeTests/ArmeeTest.java
@@ -1,6 +1,8 @@
 package batimentTypeTests;
 
 import batiment.type_batiment.Armee;
+import ressource.enumRessource;
+
 import org.junit.jupiter.api.Test;
 import tuile.Tuile;
 import tuile.type_tuile.Paturage;
@@ -8,10 +10,12 @@ import tuile.type_tuile.Paturage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+
 class ArmeeTest {
 
     @Test
-    void testArmeeCreation() {
+    public void testArmeeCreation() {
         Tuile tuile = new Paturage(1, 1);
         Armee armee = new Armee(tuile, 3);
         
@@ -21,6 +25,15 @@ class ArmeeTest {
         assertThrows(IllegalArgumentException.class, () -> new Armee(tuile, 6), "Armée doit avoir entre 1 et 5 guerriers");
     }
     
+    @Test
+    public void testArmeeCout() {
+    	Tuile tuile = new Paturage(1,1);
+    	Armee armee = new Armee(tuile,3);
+    	Map<enumRessource, Integer> cout = armee.getCout();
+    	assertEquals(1, cout.get(enumRessource.Bois));
+    	assertEquals(1, cout.get(enumRessource.Moutons));
+    	assertEquals(1, cout.get(enumRessource.Ble));
+    }
    
 
 
