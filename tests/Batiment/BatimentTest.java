198cd89654b43fb9582b0765ca857560ae577c75
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-02-21 04:05:17 +0100

    Méthode de configuration exécutée avant chaque test.

--- tests/Batiment/BatimentTest.java
+++ tests/Batiment/BatimentTest.java
@@ -1,14 +1,34 @@
 package Batiment;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import batiment.Batiment;
+import ressource.enumRessource;
+import tuile.Tuile;
+import tuile.type_tuile.Champ;
+import java.util.Map;
+import java.util.HashMap;
 
-import org.junit.jupiter.api.Test;
-
-class BatimentTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+public class BatimentTest {
+	private Batiment batiment;
+	private Tuile tuile;
+	/** Initialise un objet Batiment de test et l'associe à une Tuile.
+	 * */
+	@BeforeEach
+	void setUp() {
+		tuile = new Champ(0, 0); // Création d'une tuile de type Champ
+		batiment = new Batiment("TestBatiment", 3, tuile) {
+			@Override
+			public boolean peutEtreConstruit() {
+				return true;
+			}
+			@Override
+			public Map<enumRessource, Integer> getCout() {
+				Map<enumRessource, Integer> cout = new HashMap<>();
+				cout.put(enumRessource.Bois, 2); // Coût en bois
+				cout.put(enumRessource.Moutons, 1); // Coût en mouton
+				return cout;
+			}
+		};
 	}
 
 }
