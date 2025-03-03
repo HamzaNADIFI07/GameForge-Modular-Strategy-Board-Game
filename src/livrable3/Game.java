7105e4ad894a93817ff437db72d1e2de3bb32789
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-04 00:38:29 +0100

    utilisation de player dans Game

--- src/livrable3/Game.java
+++ src/livrable3/Game.java
@@ -5,8 +5,8 @@ import java.util.List;
 import plateau.Plateau;
 
 public abstract class Game {
-		//protected Player currentPlayer;
-	    //protected List<Player> players;  // need to create a Player class
+		protected Player currentPlayer;
+	    protected List<Player> players;  // need to create a Player class
 	    protected Plateau plateau;           // The game board
 	    protected boolean isGameActive;  // Flag to check if the game is currently active
 	    
@@ -15,7 +15,7 @@ public abstract class Game {
 	     * Initializes the game plateau and player list.
 	     */
 	    public Game() {
-	        // this.players = new ArrayList<>(); // while waiting for the creation of player
+	        this.players = new ArrayList<>(); // while waiting for the creation of player
 	        this.plateau = new Plateau(10, 10);  // Assuming default size,
 	        this.isGameActive = false;
 	    }
