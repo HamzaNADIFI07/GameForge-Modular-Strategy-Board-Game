0a206eaf15fa58fab9857d7fb075544046ffcabd
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-04 00:16:57 +0100

    constructeur de Game

--- src/livrable3/Game.java
+++ src/livrable3/Game.java
@@ -10,5 +10,15 @@ public abstract class Game {
 	    protected Plateau plateau;           // The game board
 	    protected boolean isGameActive;  // Flag to check if the game is currently active
 	    
+	    /**
+	     * Constructor for Game class.
+	     * Initializes the game plateau and player list.
+	     */
+	    public Game() {
+	        // this.players = new ArrayList<>(); // while waiting for the creation of player
+	        this.plateau = new Plateau(10, 10);  // Assuming default size,
+	        this.isGameActive = false;
+	    }
+	    
 
 }
