2a43765b0ce3e5eba8ce6404a947deb7434a8b06
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-04 00:30:01 +0100

    ajout d'un attribu et justification de l'utilisation  initializeGame();

--- src/livrable3/Game.java
+++ src/livrable3/Game.java
@@ -5,7 +5,7 @@ import java.util.List;
 import plateau.Plateau;
 
 public abstract class Game {
-	
+		//protected Player currentPlayer;
 	    //protected List<Player> players;  // need to create a Player class
 	    protected Plateau plateau;           // The game board
 	    protected boolean isGameActive;  // Flag to check if the game is currently active
@@ -19,6 +19,11 @@ public abstract class Game {
 	        this.plateau = new Plateau(10, 10);  // Assuming default size,
 	        this.isGameActive = false;
 	    }
+	    //pour obliger les classes dérivées (comme Ares et Demeter) à implémenter
+	    //leur propre logique de mise en place initiale du jeu, qui varie selon les 
+	    //règles spécifiques de chaque version du jeu.
+	    protected abstract void initializeGame();
+	    
 	    
 
 }
