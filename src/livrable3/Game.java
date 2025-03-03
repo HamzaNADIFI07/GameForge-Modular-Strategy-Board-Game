fd5ba582a3b1fdfac37a03ef95c046ea2505c5f5
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-04 00:14:21 +0100

    creating de la classe game avec des attribu

---
+++ src/livrable3/Game.java
@@ -0,0 +1,14 @@
+package livrable3;
+import java.util.ArrayList;
+import java.util.List;
+
+import plateau.Plateau;
+
+public abstract class Game {
+	
+	    //protected List<Player> players;  // need to create a Player class
+	    protected Plateau plateau;           // The game board
+	    protected boolean isGameActive;  // Flag to check if the game is currently active
+	    
+
+}
