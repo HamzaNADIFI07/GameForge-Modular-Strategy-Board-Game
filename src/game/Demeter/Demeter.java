8847f48d8d53e7c8e36b9bf3f3b6422cf5584817
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-04 14:59:04 +0100

    creation de la constructeur de Demeter

--- src/game/Demeter/Demeter.java
+++ src/game/Demeter/Demeter.java
@@ -1,5 +1,19 @@
 package game.Demeter;
 
-public class Demeter {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import game.Game;
+import game.Player;
+import plateau.Plateau;
+
+public abstract class Demeter extends Game {
+	protected Map<Player, Integer> resourceStock;
+	
+	public Demeter(List<Player> players, Plateau plateau) {
+		super(players, plateau);
+		resourceStock = new HashMap<>();
+	}
 
 }
