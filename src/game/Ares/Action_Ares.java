4b5b0125a0f4b2a16ae4fa015c2b6f6c0bec6c4f
ahmed.sylla <ahmed.sylla.etu@univ-lille.fr>
2025-03-05 11:43:18 +0100

    ajout du constructeur de la classe Action_Ares

--- src/game/Ares/Action_Ares.java
+++ src/game/Ares/Action_Ares.java
@@ -5,9 +5,19 @@ import plateau.Plateau;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import game.Game;
 import game.Player;
 
+public class Action_Ares {
+
+    private Game game;
+    private Player player;
+
+    public Action_Ares(Game game, Player player) {
+        this.game = game;
+        this.player = player;
+    }
+
+}  
 
 
