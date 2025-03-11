eb6a981c82beac51bb1cc4ed9e34e3afd14c75a5
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-11 06:39:21 +0100

    modification de la constructeur de Player

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,27 +1,34 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ressource.Ressource;
+
 public class Player {
 	private String name;
-    private int score;
+	private Map<Ressource, Integer> ressources;
+	private int warriors;
+	private boolean hasSecretWeapon;
     
+	//Ce constructeur initialise un joueur avec un nom, 
+	//lui donne 10 unités de chaque ressource, définit 
+	//son nombre de guerriers à 0 et indique qu'il ne possède pas d'arme secrète.
     public Player(String name) {
         this.name = name;
-        this.score = 0;
-    }
-    public void updateScore(int points) {
-        score += points;
+        this.ressources = new HashMap<>();
+        for (Ressource r : Ressource.values()) {
+        	this.ressources.put(r, 10);
+        }
+        this.warriors = 0;
+        this.hasSecretWeapon = false;
+        
     }
     public String getName() {
     	return name;
     }
     public void setName(String name) {
     	this.name = name;
-    }
-    public int getScore() {
-    	return score;
-    }
-    public void setScore(int score) {
-    	this.score = score;
     }
     }
 
