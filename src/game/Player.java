8e144c864ddb32097efbbd727cc548d21708d445
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-11 06:52:31 +0100

    méthode construireBatiment

--- src/game/Player.java
+++ src/game/Player.java
@@ -3,7 +3,9 @@ package game;
 import java.util.HashMap;
 import java.util.Map;
 
+import batiment.Batiment;
 import ressource.Ressource;
+import tuile.Tuile;
 
 public class Player {
 	private String name;
@@ -23,6 +25,28 @@ public class Player {
         this.warriors = 0;
         this.hasSecretWeapon = false;
         
+    }
+    /**
+     * Permet au joueur de construire un bâtiment sur une tuile donnée,
+     * en vérifiant si les ressources nécessaires sont disponibles.
+     * 
+     * @param b Le bâtiment à construire
+     * @param t La tuile sur laquelle construire le bâtiment
+     * @return
+     */
+    public void construireBatiment(Batiment b, Tuile t) {
+    	Map<Ressource, Integer> cout = b.getCout();
+    	for (Map.Entry<Ressource, Integer> entry : cout.entrySet()) {
+    		if (this.ressources.getOrDefault(entry.getKey(), 0) < entry.getValue()) {
+    			System.out.println("Ressources insuffisantes pour construire " + b.getType());
+    			return;
+    		}
+    	}
+    	for (Map.Entry<Ressource, Integer> entry : cout.entrySet()) {
+    		this.ressources.put(entry.getKey(), this.ressources.get(entry.getKey()) - entry.getValue());
+    	}
+    	t.setBatiment(b);
+    	System.out.println(name + " a construit " + b.getType() + " sur " + t);
     }
     public String getName() {
     	return name;
