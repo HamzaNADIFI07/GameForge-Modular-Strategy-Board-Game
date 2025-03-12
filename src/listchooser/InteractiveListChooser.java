6df9a82fbd67b736dddaafc9b91e1a7435223767
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-12 11:25:54 +0100

    le constructeur de listchooser

--- src/listchooser/InteractiveListChooser.java
+++ src/listchooser/InteractiveListChooser.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 public class InteractiveListChooser {
 	private Scanner scanner;
 	private Random random;
-	
+	public InteractiveListChooser() {
+		this.scanner = new Scanner(System.in);
+		this.random = new Random();
+	}
 
 }
