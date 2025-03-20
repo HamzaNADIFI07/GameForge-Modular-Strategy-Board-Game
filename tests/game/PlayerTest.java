dfc817345810ef73bd27d2953a05901a5a111d32
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-21 00:53:50 +0100

    classe de test pour player

---
+++ tests/game/PlayerTest.java
@@ -0,0 +1,14 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+	private Player player ;
+	@Test
+	void setUp() {
+		player = new Player("Leon");
+	}
+
+}
