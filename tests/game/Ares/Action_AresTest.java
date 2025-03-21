d95fe48258de4f378e89f12203792f8c754b9523
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-21 01:33:58 +0100

    creation de la classe de test de Action_Ares

---
+++ tests/game/Ares/Action_AresTest.java
@@ -0,0 +1,14 @@
+package game.Ares;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Action_AresTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
