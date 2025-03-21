338e1972e5a9a76dc75c91caa98f8bd5ff1f280c
Hamza NADIFI <nadifihamza07@gmail.com>
2025-03-21 12:51:17 +0100

    Création de la classe Action_Demeter

---
+++ src/game/Demeter/Action_Demeter.java
@@ -0,0 +1,14 @@
+package game.Demeter;
+
+import game.Game;
+import game.Player;
+
+public class Action_Demeter {
+
+    private Player player;
+
+    public Action_Demeter(Game game, Player player) {
+        this.player = player;
+    }
+
+}
