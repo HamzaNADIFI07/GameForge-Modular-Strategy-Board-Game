1883f6d69996dfbc0c1012142d0b27cbb182a104
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-21 01:04:57 +0100

    méthode pour tester les ressource initiales

--- tests/game/PlayerTest.java
+++ tests/game/PlayerTest.java
@@ -2,7 +2,12 @@ package game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.text.DateFormat.Field;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import ressource.Ressource;
 
 class PlayerTest {
 	private Player player ;
@@ -10,5 +15,13 @@ class PlayerTest {
 	void setUp() {
 		player = new Player("Leon");
 	}
+	/*
+	 * Tests if the player starts with enough resources*
+	 */
+	 void testInitialResources() {
+		 for (Ressource r : Ressource.values()) {
+			 assertTrue(player.hasResources(r, 10), "Player should start with 10 of " + r);
+		 }
+	 }
 
 }
