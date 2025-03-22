9c5b140e33db410a1bd47895323427967bc99587
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-22 17:33:42 +0100

    creation de la classe de test de action demeter

---
+++ tests/game/Demeter/Action_DemeterTest.java
@@ -0,0 +1,14 @@
+package game.Demeter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Action_DemeterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
