fec244fe9e5adc56934cc87cd4acdd05b669cfea
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-22 17:59:20 +0100

    setup method

--- tests/game/Demeter/Action_DemeterTest.java
+++ tests/game/Demeter/Action_DemeterTest.java
@@ -2,13 +2,41 @@ package game.Demeter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Action_DemeterTest {
+import game.Game;
+import game.Player;
+import plateau.Plateau;
+import tuile.Tuile;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class Action_DemeterTest {
+	private Plateau plateau;
+	private Player player;
+	private Game fakeGame;
+	private Action_Demeter actionDemeter;
+	
+	@BeforeEach
+	void setUp() {
+		plateau = new Plateau(10, 10);
+		player = new Player("leon");
+		List<Player> players = new ArrayList<>();
+		players.add(player);
+		fakeGame = new Game(players, 10, 10) {
+			@Override
+			 public Player getCurrentPlayer() {
+				 return player;
+			 }
+			@Override
+			public void initializeGame() {
+				// rien à faire ici pour le test
+			}
+		};
+		actionDemeter = new Action_Demeter(fakeGame);
+
 	}
 
 }
