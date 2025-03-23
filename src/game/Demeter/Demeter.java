bca43322ca58fa2b569445c7ae648da4b95a2bb3
Hamza NADIFI <nadifihamza07@gmail.com>
2025-03-23 17:06:35 +0100

    JavaDoc de la class Demeter

--- src/game/Demeter/Demeter.java
+++ src/game/Demeter/Demeter.java
@@ -6,18 +6,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Classe Demeter.
+ *
+ * Cette classe étend la classe abstraite Game et définit les caractéristiques spécifiques du jeu Demeter. 
+ * Elle gère le stock de ressources des joueurs et fournit un accès aux actions spécifiques au jeu via la classe Action_Demeter.
+ *
+ */
 public class Demeter extends Game {
+
 	protected Map<Player, Integer> resourceStock;
+
 	private Action_Demeter action;
 	
+	/**
+     * Constructeur de la classe Demeter.
+     *
+     * @param players Liste des joueurs participants.
+     * @param x Largeur du plateau.
+     * @param y Hauteur du plateau.
+     */
 	public Demeter(List<Player> players,int x,int y ) {
 		super(players, x,y);
 		resourceStock = new HashMap<>();
 		this.action = new Action_Demeter(this);
 	}
+	/**
+     * Initialise le jeu avec les configurations spécifiques à Demeter.
+     */
 	protected  void initializeGame(){
 		
 	}
+
+	/**
+     * Retourne l'objet permettant d'effectuer des actions spécifiques à Demeter.
+     *
+     * @return L'instance de Action_Demeter associée à ce jeu.
+     */
 	public Action_Demeter getAction() {
 		return action;
 	};
