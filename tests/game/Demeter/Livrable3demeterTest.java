b63bcee80c92737c19e7dad6ce97becbc1913c65
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-24 04:13:13 +0100

    creation de la classe de test de livrabledemeter

---
+++ tests/game/Demeter/Livrable3demeterTest.java
@@ -0,0 +1,14 @@
+package game.Demeter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Livrable3demeterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
