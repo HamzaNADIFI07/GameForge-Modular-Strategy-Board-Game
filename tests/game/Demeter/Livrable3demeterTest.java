290a5409b211f306364ef2c93b97708e9f9ee7ff
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-24 04:18:52 +0100

    creation de la méthode before each

--- tests/game/Demeter/Livrable3demeterTest.java
+++ tests/game/Demeter/Livrable3demeterTest.java
@@ -2,13 +2,39 @@ package game.Demeter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Livrable3demeterTest {
+import game.Player;
+import ressource.Ressource;
+import tuile.Tuile;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class Livrable3demeterTest {
+	private Demeter game;
+	private Player player;
+	private int fermeX = 2, fermeY = 2;
+	private int portX = 4, portY = 4;
+
+	@BeforeEach
+	void setUp() {
+		player = new Player("Leon");
+		List<Player> players = new ArrayList<>();
+		players.add(player);
+		
+		game = new Demeter(players, 10, 10);
+		game.setCurrentPlayer(player);
+		Tuile tuileFerme = game.getPlateau().getTuile(fermeX, fermeY);
+		Tuile tuilePort = game.getPlateau().getTuile(portX, portY);
+		
+		player.addTuile(tuileFerme);
+		player.addTuile(tuilePort);
+		// Remplissage généreux des ressources pour éviter tout blocage
+		for (Ressource r : Ressource.values()) {
+			player.addRessource(r, 20);
+		}
 	}
 
 }
