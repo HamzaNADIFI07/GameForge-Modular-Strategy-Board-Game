6a94c46dc93635f9d73f040a6355d8483e38f8a8
anas.chebbi.etu <anaschebbi30@gmail.com>
2025-03-24 06:01:37 +0100

    class pour tester livrable3aresTest

---
+++ src/game/Ares/Livrable3aresTest.java
@@ -0,0 +1,14 @@
+package game.Ares;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Livrable3aresTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
